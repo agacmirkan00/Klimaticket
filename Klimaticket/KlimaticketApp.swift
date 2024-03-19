e9bafbc0d3025a870e3834c0edf700f6904d8b9f
agacmirkan00 <s2210824019@fhooe.at>
2024-03-19 11:31:22 +0100

    Initial Commit

---
+++ Klimaticket/KlimaticketApp.swift
@@ -0,0 +1,17 @@
+//
+//  KlimaticketApp.swift
+//  Klimaticket
+//
+//  Created by Mirkan Agac on 19.03.24.
+//
+
+import SwiftUI
+
+@main
+struct KlimaticketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
